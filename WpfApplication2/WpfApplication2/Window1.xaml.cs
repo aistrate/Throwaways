fbb0f06d3bc6c51230e87a00804085f189b5bd64
Adrian Istrate <adrian.istrate@yahoo.com>
2010-08-16 22:57:33 +0200

    first commit

---
+++ WpfApplication2/WpfApplication2/Window1.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private DrawingVisual CreateDrawingVisualText()
+        {
+            //FontCapitals.SmallCaps
+            // Create an instance of a DrawingVisual.
+            DrawingVisual drawingVisual = new DrawingVisual();
+
+            // Retrieve the DrawingContext from the DrawingVisual.
+            DrawingContext drawingContext = drawingVisual.RenderOpen();
+
+            // Draw a formatted text string into the DrawingContext.
+            drawingContext.DrawText(
+               new FormattedText("Click Me!",
+                  CultureInfo.GetCultureInfo("en-us"),
+                  FlowDirection.LeftToRight,
+                  new Typeface("Verdana"),
+                  36, System.Windows.Media.Brushes.Black),
+                  new System.Windows.Point(200, 116));
+
+            // Close the DrawingContext to persist changes to the DrawingVisual.
+            drawingContext.Close();
+
+            return drawingVisual;
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            //drawingContext.DrawRectangle(Brushes.Crimson, null, new Rect(new Point(0, 0), new Point(500, 500)));
+            //base.OnRender(drawingContext);
+
+            //return;
+
+            string testString = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor";
+
+            // Create the initial formatted text string.
+            FormattedText formattedText = new FormattedText(
+                testString,
+                CultureInfo.GetCultureInfo("en-us"),
+                FlowDirection.LeftToRight,
+                new Typeface("Verdana"),
+                32,
+                Brushes.Black);
+
+            // Set a maximum width and height. If the text overflows these values, an ellipsis "..." appears.
+            formattedText.MaxTextWidth = 300;
+            formattedText.MaxTextHeight = 240;
+
+            // Use a larger font size beginning at the first (zero-based) character and continuing for 5 characters.
+            // The font size is calculated in terms of points -- not as device-independent pixels.
+            formattedText.SetFontSize(36 * (96.0 / 72.0), 0, 5);
+
+            // Use a Bold font weight beginning at the 6th character and continuing for 11 characters.
+            formattedText.SetFontWeight(FontWeights.Bold, 6, 11);
+
+            // Use a linear gradient brush beginning at the 6th character and continuing for 11 characters.
+            formattedText.SetForegroundBrush(
+                                    new LinearGradientBrush(
+                                    Colors.Orange,
+                                    Colors.Teal,
+                                    90.0),
+                                    6, 11);
+
+            // Use an Italic font style beginning at the 28th character and continuing for 28 characters.
+            formattedText.SetFontStyle(FontStyles.Italic, 28, 28);
+
+            // Draw the formatted text string to the DrawingContext of the control.
+            drawingContext.DrawText(formattedText, new Point(10, 0));
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            CreateDrawingVisualText();
+        }
+    }
+}
